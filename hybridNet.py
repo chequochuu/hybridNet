d1c04b47b04c59e35fb05db299b87d277f13388f
Che Huu <chequochuu@yahoo.com.vn>
2018-07-15 09:20:02 +0700

    aa

---
+++ hybridNet.py
@@ -0,0 +1,68 @@
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from load_eval import *
+
+load = True
+batch_size = 64
+lr = 1e-3
+input_dim = 1024
+output_dim = 1
+total_steps = 10000
+save_step = 1000
+hidden_dim1 = 800
+hidden_dim2 = 850
+cost_func = nn.BCELoss() #MSELoss()
+
+class hybridNN(torch.nn.Module):
+    def __init__(self, batch_size, input_dim, output_dim):
+        super(hybridNN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim1) 
+        self.fc2 = nn.Linear(hidden_dim1, hidden_dim2) 
+        self.fc3 = nn.Linear(hidden_dim2, output_dim) 
+    def forward(self, x):
+        x = F.elu(self.fc1(x))
+        x = F.elu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
+        return x
+
+def save_checkpoint(N, optim, data_dir, filename='checkpoint'):
+    state = {'Net': N.state_dict(),
+            'optim': optim.state_dict()}
+    torch.save(state, data_dir + filename)
+    
+def load_checkpoint(data_dir, filename='checkpoint'):
+    checkpoint = torch.load(data_dir + filename)
+    return checkpoint['Net'], checkpoint['optim']
+
+N = hybridNN(batch_size, 1024, 1)
+optim = torch.optim.Adam(N.parameters(), lr = lr) 
+data = Data(batch_size)
+
+if load = True 
+    stateN, stateOp = load_checkpoint(data.dataDir)
+    N.load_state_dict(stateN)
+    optim.load_state_dict(stateOp)
+
+if (torch.cuda.is_available()):
+    device = 'cuda'
+else:
+    device = 'cpu'
+N = N.to(device) 
+
+for i in tqdm(range(total_steps)):
+    embeddings, inceptionsHD, inceptionsAttn = data.next()
+    embeddings = Variable(torch.from_numpy(embeddings).float()).to(device)
+    tam = inceptionsAttn > inceptionsHD
+    results = Variable(torch.Tensor([[1] if i else [0] for i in tam])).to(device)
+    outs = N(embeddings) 
+    loss = cost_func(outs, results)
+    loss.backward()
+    optim.step()
+    N.zero_grad()
+    if (i % save_step ==0):
+        print(loss)
+        save_checkpoint(N, optim, data.dataDir) 
+
---
+++ load_eval.py
@@ -0,0 +1,91 @@
+import csv
+import numpy as np
+import json
+import pickle
+import h5py
+def formatname(arr):
+    for (i,j) in enumerate(arr):
+        x = j[0].split('_')
+        arr[i][0] = '{:0>4s}_{}'.format(x[0],x[1])
+    return arr
+
+def makePair(i,j,k):
+    return [str(i)+'_'+str(j),k]
+
+def load_eval_result(filename, datatype):
+    if (datatype == 'json'):
+        t = json.load(open(filename,'r'))['mean']
+        ret = [makePair(i,j,t[i*5+j]) for i in range(2933) for j in range(5)]
+    elif datatype == 'csv':
+        with open(filename,'r') as f:
+            ret = []
+            t = csv.reader(f)
+            for i in t:
+                ret.append(i)
+        ret=ret[1:]
+    else:
+        raise ValueError('wtf')
+
+    ret = formatname(ret)
+    return ret
+
+def load_captions(data_dir, filename = 'caption_array_extend.pickle'):
+    a = pickle.load(open(data_dir + filename,'rb'))
+    return a
+
+def load_embeddings(data_dir, filename = 'original.h5'):
+    f = h5py.File(data_dir+ filename, 'r')
+    ret = list(f['embedding'])
+    ret = np.array(ret)
+    return ret
+
+class Data():
+    def __init__(self, batch_size, data_dir = 'Data/'):
+        self.dataDir = data_dir
+        self.evalAttn = load_eval_result(data_dir + 'eval_2933_5_tien.csv', 'csv')
+        self.evalHD = load_eval_result(data_dir + 'birds_256_G_epoch_500_inception_score.json', 'json')
+        self.id = np.array([i[0] for i in self.evalHD])
+        self.evalHD = np.array([i[1] for i in self.evalHD])
+        self.evalHD = self.evalHD.astype(np.float32)
+        self.evalAttn = np.array([i[1] for i in self.evalAttn])
+        self.evalAttn = self.evalAttn.astype(np.float32)
+        self.total = self.evalHD.__len__()
+        self.ntest = int(self.total*1/5)
+        self.ntrain = self.total - self.ntest
+        self.captions = load_captions(self.dataDir)  
+        self.embeddings = load_embeddings(self.dataDir)
+        self.alreadySortedIndex = False
+        self.batch_size = batch_size
+        self.index = 0
+        np.random.seed(1234)
+        self.permutation = np.random.permutation(self.total)
+
+#    def sortIndex(self):
+#        self.evalAttn = sorted(self.evalAttn, key = lambda entry: entry[0]) 
+#        self.evalHD = sorted(self.evalHD, key = lambda entry: entry[0]) 
+#        self.alreadySortedIndex = True
+#
+#    def sortValue_1col(col):
+#        if col == 'HD':
+#            sorted(self.evalHD, key = lambda entry: entry[1]) 
+#        elif col == 'ATTN' :
+#            sorted(self.evalAttn, key = lambda entry: entry[1]) 
+
+
+#    def getSentence(index, captions):
+#        t = index.split('_')
+#        return captions[int(t[0])][int(t[1])] 
+
+    def next(self):
+        start = self.index
+        end = self.index + self.batch_size
+        if end > self.total:
+            start = 0
+            end = self.batch_size
+        self.index = end
+        idx = self.permutation[start:end]
+        return self.embeddings[idx], self.evalHD[idx], self.evalAttn[idx]
+
+#    def getEmbedding(self, index)
+    
+
